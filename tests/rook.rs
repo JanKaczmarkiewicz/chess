d42cbb3ebfffc829d6d4dbc3dd757a8df0dc26ec
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-07-09 23:22:22 +0200

    test: add rook tests

---
+++ tests/rook.rs
@@ -0,0 +1,93 @@
+mod common;
+
+use chess::core::state::{
+    PossibleMove,
+    PossibleMoveKind::{Capture, Move},
+};
+use common::{from_literal, to_literal};
+use std::vec;
+
+#[test]
+fn rook_initial_move() {
+    let mut state = from_literal(
+        "
+  0 1 2 3 4 5 6 7
+0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+2 . . . . . . . .
+3 . . . . . . . .
+4 . . . ♖ . . . .
+5 . . . . . . . .
+6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖",
+    );
+
+    state.handle_action((3, 4));
+
+    assert_eq!(
+        state.possible_moves,
+        vec![
+            PossibleMove {
+                kind: Move,
+                coordinate: (4, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (5, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (6, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (7, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (3, 5)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (3, 3)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (3, 2)
+            },
+            PossibleMove {
+                kind: Capture,
+                coordinate: (3, 1)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (2, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (1, 4)
+            },
+            PossibleMove {
+                kind: Move,
+                coordinate: (0, 4)
+            }
+        ]
+    );
+
+    state.handle_action((6, 4));
+
+    assert_eq!(state.possible_moves, vec![]);
+    assert_eq!(
+        to_literal(&state),
+        "
+  0 1 2 3 4 5 6 7
+0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+2 . . . . . . . .
+3 . . . . . . . .
+4 . . . . . . ♖ .
+5 . . . . . . . .
+6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖"
+    );
+}
