7b3696aad7827511842b598e8f35e8767742d1f7
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-07-10 09:43:05 +0200

    refactor: rook tests

--- tests/rook.rs
+++ tests/rook.rs
@@ -9,18 +9,19 @@ use pretty_assertions::assert_eq;
 use std::vec;
 
 #[test]
-fn basic_move() {
+fn possible_moves() {
     let mut state = from_literal(
         "
-  0 1 2 3 4 5 6 7
-0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-2 . . . . . . . .
-3 . . . . . . . .
-4 . . . ♖ . . . .
-5 . . . . . . . .
-6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖",
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . ♖ . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        ",
     );
 
     state.select_tile((3, 4));
@@ -74,21 +75,40 @@ fn basic_move() {
             }
         ]
     );
+}
 
+#[test]
+fn basic_move() {
+    let mut state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . ♖ . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        ",
+    );
+
+    state.select_tile((3, 4));
     state.select_tile((6, 4));
 
     assert_eq!(
         to_literal(&state),
         "
-  0 1 2 3 4 5 6 7
-0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-2 . . . . . . . .
-3 . . . . . . . .
-4 . . . . . . ♖ .
-5 . . . . . . . .
-6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖"
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . ♖ .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        "
     );
 }
 
@@ -96,82 +116,32 @@ fn basic_move() {
 fn capture() {
     let mut state = from_literal(
         "
-  0 1 2 3 4 5 6 7
-0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-2 . . . . . . . .
-3 . . . . . . . .
-4 . . . ♖ . . . .
-5 . . . . . . . .
-6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖",
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . ♖ . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        ",
     );
 
     state.select_tile((3, 4));
-
-    assert_eq!(
-        state.possible_moves,
-        vec![
-            PossibleMove {
-                kind: Move,
-                coordinate: (4, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (5, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (6, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (7, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (3, 5)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (3, 3)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (3, 2)
-            },
-            PossibleMove {
-                kind: Capture,
-                coordinate: (3, 1)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (2, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (1, 4)
-            },
-            PossibleMove {
-                kind: Move,
-                coordinate: (0, 4)
-            }
-        ]
-    );
-
     state.select_tile((3, 1));
 
     assert_eq!(
         to_literal(&state),
         "
-  0 1 2 3 4 5 6 7
-0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-1 ♟ ♟ ♟ ♖ ♟ ♟ ♟ ♟
-2 . . . . . . . .
-3 . . . . . . . .
-4 . . . . . . . .
-5 . . . . . . . .
-6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖"
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♖ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . ♘ ♗ ♕ ♔ ♗ ♘ ♖"
     );
 }
