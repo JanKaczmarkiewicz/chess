8f53cbc11f18962b3c7af7cb02812b0e1c96bba9
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-07-13 22:18:19 +0200

    test: king left basic castle

---
+++ tests/king_castle.rs
@@ -0,0 +1,39 @@
+mod common;
+
+use common::{from_literal, to_literal};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn castle_left() {
+    let mut state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 ♖ . . . ♔ ♗ ♘ ♖
+        ",
+    );
+
+    state.select_tile((4, 7));
+    state.select_tile((2, 7));
+
+    assert_eq!(
+        to_literal(&state),
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 . . ♔ ♖ . ♗ ♘ ♖
+        "
+    );
+}
