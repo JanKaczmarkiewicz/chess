0b6ed4e4a3cc5a436ba37f7e6bb4400fa20e0764
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-07-13 23:05:25 +0200

    test: castle rook moved

--- tests/king_castle.rs
+++ tests/king_castle.rs
@@ -91,6 +91,58 @@ fn castle_left_king_moved() {
 }
 
 #[test]
+fn castle_left_rook_moved() {
+    let mut state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 ♖ . . . ♔ ♗ ♘ ♖
+        ",
+    );
+
+    // white rook move
+    state.select_tile((0, 7));
+    state.select_tile((1, 7));
+
+    // black move
+    state.select_tile((6, 1));
+    state.select_tile((6, 2));
+
+    // white rook move (return to initial position)
+    state.select_tile((1, 7));
+    state.select_tile((0, 7));
+
+    // black move
+    state.select_tile((6, 2));
+    state.select_tile((6, 3));
+
+    // white king tries castle
+    state.select_tile((4, 7));
+    state.select_tile((2, 7));
+
+    assert_eq!(
+        to_literal(&state),
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ . ♟
+        2 . . . . . . . .
+        3 . . . . . . ♟ .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 ♖ . . . ♔ ♗ ♘ ♖
+        "
+    );
+}
+
+#[test]
 fn castle_right() {
     let mut state = from_literal(
         "
@@ -152,6 +204,58 @@ fn castle_right_king_moved() {
     // white king move (return to initial position)
     state.select_tile((5, 7));
     state.select_tile((4, 7));
+
+    // black move
+    state.select_tile((6, 2));
+    state.select_tile((6, 3));
+
+    // white king tries castle
+    state.select_tile((4, 7));
+    state.select_tile((6, 7));
+
+    assert_eq!(
+        to_literal(&state),
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ . ♟
+        2 . . . . . . . .
+        3 . . . . . . ♟ .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 ♖ ♘ ♗ ♕ ♔ . . ♖
+        "
+    );
+}
+
+#[test]
+fn castle_right_rook_moved() {
+    let mut state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . . . .
+        4 . . . . . . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+        7 ♖ ♘ ♗ ♕ ♔ . . ♖
+        ",
+    );
+
+    // white king move
+    state.select_tile((7, 7));
+    state.select_tile((6, 7));
+
+    // black move
+    state.select_tile((6, 1));
+    state.select_tile((6, 2));
+
+    // white rook move (return to initial position)
+    state.select_tile((6, 7));
+    state.select_tile((7, 7));
 
     // black move
     state.select_tile((6, 2));
