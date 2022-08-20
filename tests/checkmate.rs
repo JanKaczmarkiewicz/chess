a61de15ec273fb013eb11473aa59920cc1fb55e9
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-08-20 13:22:49 +0200

    test: checkmate

---
+++ tests/checkmate.rs
@@ -0,0 +1,41 @@
+mod common;
+
+use common::from_literal;
+
+#[test]
+fn basic_checkmate() {
+    let state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ . . ♟
+        2 . . . . . . . .
+        3 . . . . . ♟ ♟ ♕
+        4 . . . . ♙ . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ . ♙ ♙ ♙
+        7 ♖ ♗ ♘ . ♔ ♗ ♘ ♖
+        ",
+    );
+
+    assert!(state.is_checkmate());
+}
+
+#[test]
+fn covering() {
+    let state = from_literal(
+        "
+          0 1 2 3 4 5 6 7
+        0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+        1 ♟ ♟ ♟ ♟ ♟ . ♟ ♟
+        2 . . . . . . . .
+        3 . . . . . ♟ . ♕
+        4 . . . . ♙ . . .
+        5 . . . . . . . .
+        6 ♙ ♙ ♙ ♙ . ♙ ♙ ♙
+        7 ♖ ♗ ♘ . ♔ ♗ ♘ ♖
+        ",
+    );
+
+    assert!(!state.is_checkmate());
+}
